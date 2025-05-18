b85ef5e4e0fff17541c80c7db8384e82639a8026
education-bootcamp <education.seekerscloud@gmail.com>
2025-05-18 09:43:26 +0530

    1

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const UserSchema = require('../model/UserSchema');
+const jwt = require('jsonwebtoken');
 // register
 const register = async (req, resp)=>{
     try{
@@ -28,3 +29,30 @@ const register = async (req, resp)=>{
     }
 }
 // login
+const login = async (req, resp)=>{
+    try{
+        const {username, password}= req.body;
+        if(!username || !password){
+            return resp.status(400).json({'message':'some fields are missing!..'});
+        }
+
+        const user = await UserSchema.findOne({username});
+        if(!user){
+            return resp.status(404).json({'message':'User not found!..'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            return resp.status(401).json({'message':'password is wrong!..'});
+        }
+
+        const token  = jwt.sign({id:user._id, username:user.username},
+            process.env.JWT_SECRET || 'kljsadjashfroiewqrhnfdkadsnfasjdliajdiasjdoiasjdsakjndasjdoajdoiajdoisd',
+            {expiresIn: '24h'}
+        );
+        return resp.status(200).json({message:'Success!', token})
+
+    }catch (e) {
+        resp.status(500).json({'message':'try again', error:e});
+    }
+}
